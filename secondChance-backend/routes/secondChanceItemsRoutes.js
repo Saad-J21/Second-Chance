5ea6be72b76ecf7bebfe45c5cbdaf8adc62f7540
unknown <saaddeveloper21@gmail.com>
2024-08-12 13:48:06 +0300

    linted secondChanceitemsRoutes.js

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -45,15 +45,15 @@ router.post('/', upload.single('file'), async (req, res, next) => {
     // Step 3: task 2 - insert code here
     const collection = db.collection('secondChanceItems')
     // Step 3: task 3 - insert code here
-    let secondChanceItem = req.body
+    const  secondChanceItem = req.body
     // Step 3: task 4 - insert code here
     const lastItemQuery = await collection.find().sort({ id: -1 }).limit(1)
     await lastItemQuery.forEach((item) => {
       secondChanceItem.id = (parseInt(item.id) + 1).toString()
     })
     // Step 3: task 5 - insert code here
-    const date_added = Math.floor(new Date().getTime() / 1000)
-    secondChanceItem.date_added = date_added
+    const dateAdded = Math.floor(new Date().getTime() / 1000)
+    secondChanceItem.dateAdded = dateAdded
     res.status(201).json(secondChanceItem.ops[0])
   } catch (e) {
     next(e)
